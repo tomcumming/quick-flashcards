d065fb4f222ff3301067f6ba84cfd0767c6dce84
Tom Cumming <mail@tomcumming.co.uk>
2020-01-11 14:40:33 +0000

    Add react deps

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import { createElement } from "react";
+import { render } from "react-dom";
+
+import App from "./components/app";
+
+function start() {
+  const wrapper = document.querySelector("#wrapper");
+  if (wrapper === null) throw new Error("Can't find wrapper");
+
+  render(createElement(App), wrapper);
+}
+
+start();
